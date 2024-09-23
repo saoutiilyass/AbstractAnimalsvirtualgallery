df120aad7ad96ad80db385d9fbabf3d2974fc66e
saoutiilyass <gameur.moin@gmail.com>
2024-09-23 18:43:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById('loup').addEventListener('click', function() {
+    window.location.href = 'loup.html';
+});
+
+document.getElementById('lion').addEventListener('click', function() {
+    window.location.href = 'lion.html';
+});
+
+document.getElementById('grenouille').addEventListener('click', function() {
+    window.location.href = 'grenouille.html';
+});
+
+document.getElementById('paon').addEventListener('click', function() {
+    window.location.href = 'paon.html';
+});
+
+document.getElementById('renard').addEventListener('click', function() {
+    window.location.href = 'renard.html';
+});
+
+document.getElementById('crocodile').addEventListener('click', function() {
+    window.location.href = 'crocodile.html';
+});
+
+document.getElementById('leopard').addEventListener('click', function() {
+    window.location.href = 'leopard.html';
+});
+
+document.getElementById('aigle').addEventListener('click', function() {
+    window.location.href = 'aigle.html';
+});
+
+document.getElementById('orque').addEventListener('click', function() {
+    window.location.href = 'orque.html';
+});
+
+document.getElementById('tigre').addEventListener('click', function() {
+    window.location.href = 'tigre.html';
+});
+
+
+
