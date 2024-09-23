d6d7db6a3b1c99cc4c07985d58d074e2dd9734e0
saoutiilyass <gameur.moin@gmail.com>
2024-09-23 18:42:13 +0200

    Add files via upload

---
+++ orque.js
@@ -0,0 +1,9 @@
+function showDetail() {
+    document.getElementById('detail-view').style.display = 'block';
+}
+
+function closeDetail() {
+    document.getElementById('detail-view').style.display = 'none';
+}
+
+
